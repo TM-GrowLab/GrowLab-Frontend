20f69357e4e3ee306f8d88634c008722eddd7ee3
Marlène Braem <marlenebraem@hotmail.be>
2024-05-28 14:31:38 +0200

    Logout button + NavBar LoggedIn change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
-// import imgHeader from "../images/Header_img.png";
-import logoGrowLab from "../images/LogoGrowLab.png";
-import { useNavigate } from "react-router-dom";
-import { useFetchUserProfile } from "../hooks/useFetchUserProfile";
-
+import React from 'react';
+import logoGrowLab from '../images/LogoGrowLab.png';
+import { useNavigate } from 'react-router-dom';
+import { useFetchUserProfile } from '../hooks/useFetchUserProfile';
+import {LogoutButton} from './LogoutButton';
 
 export const NavBar = () => {
-    const { userProfile, userProfileStatus, userProfileError} = useFetchUserProfile();
+    const { userProfile, userProfileStatus, userProfileError } = useFetchUserProfile();
 
     const navigate = useNavigate();
 
@@ -20,36 +20,41 @@ export const NavBar = () => {
     const handleNavigateClients = () => {
         navigate('/clients');
     }
-    
+
     const navigateToMyProfile = () => {
-        navigate('/myProfile')
+        navigate('/myProfile');
     }
 
     const navigateTologin = () => {
-        navigate('/login')
+        navigate('/login');
     }
 
     return (
         <>
-            <div>
-                <h1>Hier data test</h1>
-                <p>{userProfile?.sub}</p>
-                <p>{userProfile?.username}</p>
-            </div>
-                <nav>
-                    <ul id="nav_items">
-                        <li>
-                            <img className="logo" src={logoGrowLab} alt="logo" />
-                        </li>
-                        <li className="nav_light" onClick={handleNavigateCommunity} >Community</li>
-                        <li className="nav_light" onClick={handleNavigateLearning}>Learning</li>
-                        <li className="nav_light" onClick={handleNavigateClients}>Clients</li>
-                    </ul>
+            <nav>
+                <ul id="nav_items">
+                    <li>
+                        <img className="logo" src={logoGrowLab} alt="logo" />
+                    </li>
+                    <li className="nav_light" onClick={handleNavigateCommunity}>Community</li>
+                    <li className="nav_light" onClick={handleNavigateLearning}>Learning</li>
+                    <li className="nav_light" onClick={handleNavigateClients}>Clients</li>
+                </ul>
+                {userProfile?.sub ? (
+                    <>
+                        <span>Welkom {userProfile.username}</span>
+                        <ul id="nav_buttons">
+                            <button id="myProfile" className="pri_button" onClick={navigateToMyProfile}>Mijn Profiel</button>
+                            <LogoutButton />
+                        </ul>
+                    </>
+                ) : (
                     <ul id="nav_buttons">
                         <button id="aanmelden" className="sec_button" onClick={navigateTologin}>Aanmelden</button>
                         <button id="registeren" className="pri_button">Registeren</button>
                     </ul>
-                </nav>
+                )}
+            </nav>
         </>
     );
 }
